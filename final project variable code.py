66591dbd24e13d9e6c64d07b7081d91e50664772
akmaccuish <89708503+akmaccuish@users.noreply.github.com>
2021-12-08 15:29:16 -0500

    Update final project variable code.py

--- final project variable code.py
+++ final project variable code.py
@@ -21,6 +21,7 @@ r = gp.Model("Model")
 # Variables
 zvars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "z") # units of Flugels produced by plant i in year t
 xvars = r.addVars(plants, warehouses, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "x") # units of Flugels shipped from plant i to warehouse j in year t
+ivars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "ivar")
 yvars = r.addVars(warehouses, stores, years, vtype=GRB.CONTINUOUS, obj=whtocust, name = "y") # units of Flugels shipped from warehouse j to retail center k in year t
 lamda1vars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "lamda1") # calculating the weighted units of purchased raw materials of plant i in year t
 lamda2vars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = planttowh, name = "lamda2") # calculating the weighted units of purchased raw materials of plant i in year t
@@ -37,16 +38,27 @@ hvars = r.addVars(plants, years, vtype=GRB.CONTINUOUS, obj = conscost, lb = 0, u
 r.addConstrs((gvars[i][t] == 1 - pvars[i][t-1] for i in plants for t in years), "Reopening cost binary variable")
 r.addConstrs((fvars[i][t] == 1 - pvars[i][t+1] for i in plants for t in years), "Shut down cost binary variable")
 r.addConstrs((zvars[i][t] <= pvars[i][t] * capacity[i] for i in plants for t in years), "Plant capacity binary variable")
+r.addConstrs((pvars[j][0] = 0 for j in warehouses), 'Year 0 P variable')
+r.addConstrs((pvars[j][11] = 0 for j in warehouses), 'Year 11 P variable')
 # Shipping Cost
 r.addConstrs((xvars.sum(i,'*',t) == zvars[i][t] for i in plants for t in years), "Sum of Products Shipped") # sum of products shipped from each plant in year t equals to the amount of flugels produced by it in year t 
-r.addConstrs((xvars.sum('*',j,t) == yvars.sum(j,'*',t) for j in warehouses for t in years), "Sum of Products Recieved at Warehouses") # sum of products received by warehouse j in year t equals to sum of products shipped from warehouse j in year t
-r.addConstrs((xvars.sum('*',j,'*') - yvars.sum(j,k,'*') <= 40000 for j in warehouses for k in stores), "Average Inventory") # average inventory in any year to be no more than 4000 items (among all Warehouses)
+r.addConstrs((xvars.sum('*',j,t) + ivars[j][t-1] == yvars.sum(j,'*',t) + ivars[j][t] for j in warehouses for t in years), "Sum of Products Recieved at Warehouses") # sum of products received by warehouse j in year t equals to sum of products shipped from warehouse j in year t
+r.addConstrs((ivars.sum(j,'*') <= 40000 for j in warehouses), "Average Inventory") # average inventory in any year to be no more than 4000 items (among all Warehouses)
 r.addConstrs((xvars.sum('*',j,t) <= 12000 for j in warehouses for t in years), "Warehouse Flow") # both the flow into a warehouse and the flow out of a warehouse should not exceed an average of 1000 units per month
 r.addConstrs((yvars.sum(j,'*',t) <= 12000 for j in warehouses for t in years), "Warehouse Flow") # both the flow into a warehouse and the flow out of a warehouse should not exceed an average of 1000 units per month
+r.addConstrs((ivars[j][0] = 0 for j in warehouses), 'Year 0 Inventory')
+r.addConstrs((ivars[j][11] = 0 for j in warehouses), 'Year 11 Inventory')
 # Demand
 r.addConstrs((yvars.sum('*',k,t) <= demand[k][t] for k in stores for t in years), "Meet Demand")
 # Alloy
 r.addConstrs((4.7 * zvars[i][t] <= 60000 for i in plants for t in years), "Pounds of Alloy")
+# Material
+r.addConstrs((lamda1vars[i][t] <= e1vars[i][t] for i in plants for t in years), "Lamda1")
+r.addConstrs((lamda2vars[i][t] <= e1vars[i][t] + e2vars[i][t] for i in plants for t in years), "Lamda2")
+r.addConstrs((lamda3vars[i][t] <= e2vars[i][t] for i in plants for t in years), "Lamda3")
+r.addConstrs((lamda1vars[i][t] + lamda2vars[i][t] + lamda3vars[i][t] == 1 for i in plants for t in years), "Lamda Sum")
+r.addConstrs((e1vars[i][t] + e2vars[i][t] == 1 for i in plants for t in years), "e1 and e2 value")
+r.addConstrs((((lamda1vars[i][t] + 9000*(lamda2vars[i][t]) + 1000000*(lamda3vars[i][t])) / 3) == zvars[i][t] for i in plants for t in years), "Z value")
 
 
 
